8fa89ba5382cf1cfb4d4c1773d1c058ff7111543
kkotto <ksen.sharupich.work@gmail.com>
2022-09-27 21:30:28 +0300

    Task 1 completed

---
+++ src/main/java/org/example/service/impl/TaskOneServiceImpl.java
@@ -0,0 +1,54 @@
+package org.example.service.impl;
+
+import org.example.service.TaskService;
+
+import java.util.*;
+
+public class TaskOneServiceImpl implements TaskService {
+    private final int NUMBER_QUANTITY=50;
+    private final int MAX_VALUE=20;
+    private final int MIN_MATCH=1;
+
+    @Override
+    public void runTask() {
+        List<Integer> list = createList();
+        printList(list);
+        Map<Integer, Integer> map = createMap(list);
+        printMap(map);
+    }
+
+    public void printMap(Map<Integer, Integer> map){
+        for(Map.Entry<Integer, Integer> item : map.entrySet()){
+            System.out.println("Number "+item.getKey()+" occurs "+item.getValue()+" times.");
+        }
+    }
+
+    public Map<Integer, Integer> createMap(List<Integer> list){
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<NUMBER_QUANTITY; i++){
+            int number=list.get(i);
+            if (map.containsKey(number)){
+                map.put(number,map.get(number)+MIN_MATCH);
+            } else {
+                map.put(number, MIN_MATCH);
+            }
+        }
+        return map;
+    }
+
+    public List<Integer> createList(){
+        List<Integer> list = new ArrayList<>();
+        Random random = new Random();
+        for(int i=0; i<NUMBER_QUANTITY; i++){
+            list.add(i, random.nextInt(MAX_VALUE));
+        }
+        return list;
+    }
+
+    public void printList(List<Integer> list){
+        for(Integer num : list){
+            System.out.print(num+" ");
+        }
+        System.out.println();
+    }
+}
