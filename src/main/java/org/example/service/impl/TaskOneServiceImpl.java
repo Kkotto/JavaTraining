6f2515af16673afbe0d64973fc80893f9ca13d4b
kkotto <ksen.sharupich.work@gmail.com>
2022-10-04 16:11:57 +0300

    Task 1 createMap fixed with forEach method

--- src/main/java/org/example/service/impl/TaskOneServiceImpl.java
+++ src/main/java/org/example/service/impl/TaskOneServiceImpl.java
@@ -5,9 +5,9 @@ import org.example.service.TaskService;
 import java.util.*;
 
 public class TaskOneServiceImpl implements TaskService {
-    private final int NUMBER_QUANTITY=50;
-    private final int MAX_VALUE=20;
-    private final int MIN_MATCH=1;
+    private final int NUMBER_QUANTITY = 50;
+    private final int MAX_VALUE = 20;
+    private final int MIN_MATCH = 1;
 
     @Override
     public void runTask() {
@@ -17,37 +17,30 @@ public class TaskOneServiceImpl implements TaskService {
         printMap(map);
     }
 
-    public void printMap(Map<Integer, Integer> map){
-        for(Map.Entry<Integer, Integer> item : map.entrySet()){
-            System.out.println("Number "+item.getKey()+" occurs "+item.getValue()+" times.");
+    public void printMap(Map<Integer, Integer> map) {
+        for (Map.Entry<Integer, Integer> item : map.entrySet()) {
+            System.out.println("Number " + item.getKey() + " occurs " + item.getValue() + " times.");
         }
     }
 
-    public Map<Integer, Integer> createMap(List<Integer> list){
+    public Map<Integer, Integer> createMap(List<Integer> list) {
         Map<Integer, Integer> map = new HashMap<>();
-        for(int i=0; i<NUMBER_QUANTITY; i++){
-            int number=list.get(i);
-            if (map.containsKey(number)){
-                map.put(number,map.get(number)+MIN_MATCH);
-            } else {
-                map.put(number, MIN_MATCH);
-            }
-        }
+        list.forEach(number -> map.put(number, map.containsKey(number) ? map.get(number) + MIN_MATCH : MIN_MATCH));
         return map;
     }
 
-    public List<Integer> createList(){
+    public List<Integer> createList() {
         List<Integer> list = new ArrayList<>();
         Random random = new Random();
-        for(int i=0; i<NUMBER_QUANTITY; i++){
+        for (int i = 0; i < NUMBER_QUANTITY; i++) {
             list.add(i, random.nextInt(MAX_VALUE));
         }
         return list;
     }
 
-    public void printList(List<Integer> list){
-        for(Integer num : list){
-            System.out.print(num+" ");
+    public void printList(List<Integer> list) {
+        for (Integer num : list) {
+            System.out.print(num + " ");
         }
         System.out.println();
     }
