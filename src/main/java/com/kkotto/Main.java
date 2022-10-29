0cdf148ab9856f81c79a7439c2d62eb3b9d7942f
kkotto <ksen.sharupich.work@gmail.com>
2022-10-29 20:39:31 +0300

    Task 2 breaking on lines

--- src/main/java/com/kkotto/Main.java
+++ src/main/java/com/kkotto/Main.java
@@ -1,9 +1,11 @@
 package com.kkotto;
 
 import com.kkotto.service.impl.TaskOneServiceImpl;
+import com.kkotto.service.impl.TaskTwoServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        new TaskOneServiceImpl().runTask();
+        //new TaskOneServiceImpl().runTask();
+        new TaskTwoServiceImpl().runTask();
     }
 }
---
+++ src/main/java/com/kkotto/service/impl/TaskTwoServiceImpl.java
@@ -0,0 +1,84 @@
+package com.kkotto.service.impl;
+
+import com.kkotto.service.TaskService;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class TaskTwoServiceImpl implements TaskService {
+    private final String RESULT_FILE_NAME = "Task-2-result.txt";
+    private final String RESULT_FILE_PATH = "src\\main\\java\\com\\kkotto\\" + RESULT_FILE_NAME;
+    private final String ORIGINAL_FILE_NAME = "Task-2.txt";
+    private final String ORIGINAL_FILE_PATH = ORIGINAL_FILE_NAME;
+    private final int MIN_LENGTH = 3;
+    private final int MAX_LENGTH = 5;
+    private final String REGEX = "[.,*:;\s]";
+    //Стоит ли использовать Matcher & Pattern?
+
+    @Override
+    public void runTask() {
+        File file = createFile(ORIGINAL_FILE_PATH);
+        List<String> text = readFile(file);
+        if (!text.isEmpty()) {
+            text = splitWords(text);
+            List<String> words = deleteWords(text);
+            File resultFile = createFile(RESULT_FILE_PATH);
+            writeToFile(resultFile, words);
+        }
+    }
+
+    private File createFile(String filePath) {
+        File file = new File(filePath);
+        try {
+            if (file.createNewFile()) {
+                System.out.println(file.getName() + " was successfully created.");
+            } else {
+                System.out.println(file.getName() + " already exists.");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return file;
+    }
+
+    private List<String> readFile(File file) {
+        List<String> lines = new ArrayList<>();
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNext()) {
+                lines.add(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        return lines;
+    }
+
+    private void writeToFile(File file, List<String> list) {
+        try (FileWriter writer = new FileWriter(file, false)) {
+            writer.write(list.toString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private List<String> splitWords(List<String> lines) {
+        return lines
+                .stream()
+                .map(s -> (Arrays.toString(s.split(REGEX))))
+                .collect(Collectors.toList());
+    }
+
+    private List<String> deleteWords(List<String> words) {
+        return words
+                .stream()
+                .filter(s -> s.length() <= MIN_LENGTH || s.length() >= MAX_LENGTH)
+                .collect(Collectors.toList());
+    }
+}
